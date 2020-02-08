cfbf2560b881044166f0e45da96b35a97d31fd7f
Aleksie Dudchenko <alexey.dudchenko@gmail.com>
2020-02-08 17:57:42 +0100

    dev. Factory Method initial commit

---
+++ src/main/java/factorymethod/App.java
@@ -0,0 +1,9 @@
+package factorymethod;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("\nThis is an example of Factory Method design pattern");
+    }
+}
