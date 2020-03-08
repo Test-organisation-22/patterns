e2e175c43affb824a19aa2e5c1d4c39d947da695
Aleksie Dudchenko <alexey.dudchenko@gmail.com>
2020-03-08 14:25:11 +0100

    dev. start of Factory method

---
+++ src/main/java/factorymethod/App.java
@@ -0,0 +1,14 @@
+package factorymethod;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("\nThis is an example of Factory Method design pattern");
+
+        
+
+
+
+    }
+}
