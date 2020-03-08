5c82af867c3acadd7e8d9e35e686ea806929caa0
Aleksie Dudchenko <alexey.dudchenko@gmail.com>
2020-03-08 17:16:44 +0100

    dev. Frame Addition

---
+++ src/main/java/codeexamples/gui/Addition.java
@@ -0,0 +1,23 @@
+package codeexamples.gui;
+
+import javax.swing.*;
+
+public class Addition {
+
+    public static void main(String[] args){
+
+        String firstNumber = JOptionPane.showInputDialog("Enter an integer");
+        String secondNumber = JOptionPane.showInputDialog("Enter second integer");
+
+        int number1, number2;
+
+        try {
+            number1 = Integer.parseInt(firstNumber);
+            number2 = Integer.parseInt(secondNumber);
+            int sum = number1 + number2;
+            JOptionPane.showMessageDialog(null, "The sum is " + sum, "Sum", JOptionPane.PLAIN_MESSAGE);
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "probably that was not numbers");
+        }
+    }
+}
